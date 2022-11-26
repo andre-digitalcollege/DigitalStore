9be3b47eaacf0b94464e2596108a7cf582d2d7fe
unknown <thericardoguilherme@gmail.com>
2022-11-26 17:11:17 -0300

    oi

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+
+  return <header>
+    <div>logo</div>
+    <div>busca</div>
+  </header>
+
+}
