db5c004087ff5aebb1f522395975aaee45975334
Igor Queiroz <igorqueirozdossantoslima_@hotmail.com>
2022-11-29 19:09:24 -0300

    imgs banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,6 +3,8 @@ import './banner.css'
 export default function Banner() {
     return (
         <div className="container">
+            <span className='ornamento-11'>Ornament-11</span>
+            <span className='shoe'>Shoe</span>
             <p className='mop' >Melhores ofertas personalizadas</p>
             <p className='qsn'>Queima de stoque Nike 🔥</p>
             <p className='texto'>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur</p>
