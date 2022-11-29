ffde19521a58f5996e7402f55fe7a8ac6530de10
Igor <igorqueirozdossantoslima_@hotmail.com>
2022-11-29 12:32:23 -0300

    parte de melhores ofertas e queima de stoque

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,8 @@ import './banner.css'
 export default function Banner() {
     return (
         <div className="container">
-            <span><p>Banner</p></span>
+            <p className='mop' >Melhores ofertas personalizadas</p>
+            <p className='qsn'>Queima de stoque Nike 🔥</p>
             <div className="carousel">
                 <div className="">
                 </div>
