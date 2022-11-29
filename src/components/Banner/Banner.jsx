be84375d4b4637f1366e04341c012e0a801c84d7
Igor <igorqueirozdossantoslima_@hotmail.com>
2022-11-29 13:47:52 -0300

    botão ver oferta

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,6 +5,8 @@ export default function Banner() {
         <div className="container">
             <p className='mop' >Melhores ofertas personalizadas</p>
             <p className='qsn'>Queima de stoque Nike 🔥</p>
+            <p className='texto'>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur</p>
+            <button className='bnt-verOferta'>Ver Oferta</button>
             <div className="carousel">
                 <div className="">
                 </div>
