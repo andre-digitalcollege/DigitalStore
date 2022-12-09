466fb3969b0ac4a207d097717d22ca1b5db99005
Igor Queiroz <igorqueirozdossantoslima_@hotmail.com>
2022-12-09 19:37:37 -0300

    jsx carousel

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 import './banner.css'
 import WhiteSneakers from './img/White-Sneakers-PNG-Clipart 1.png'
 import Ornament11 from './img/Ornament 11.png'
+import Carousel from './carousel'
 
 export default function Banner() {
     return (
-        <div className="container">
-            <img src={Ornament11} alt="Ornament" className='ornamento-11' />
-            <img src={WhiteSneakers} alt="Shoe" className='shoe' />
-            <p className='mop' >Melhores ofertas personalizadas</p>
-            <p className='qsn'>Queima de stoque Nike 🔥</p>
-            <p className='texto'>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur</p>
-            <button className='bnt-verOferta'>Ver Oferta</button>
-            <div className="carousel">
-                <div className="">
+        <div className='container'>
+            <div className="info">
+                <div>
+                    <p className='mop' >Melhores ofertas personalizadas</p>
+                    <p className='qsn'>Queima de stoque Nike 🔥</p>
+                    <p className='texto'>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur</p>
+                    <button className='bnt-verOferta'>Ver Oferta</button>
+                </div>
+                <div>
+                    <img src={WhiteSneakers} alt="Shoe" className='shoe' />
+                </div>
+                <div>
+                    <img src={Ornament11} alt="Ornament" className='ornamento-11' />
                 </div>
             </div>
+            <Carousel />
         </div>
     );
 }
