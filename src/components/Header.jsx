19d253f8d1c89700e3b4e35b53f86a3fe71c0346
deivid lima <deividlima1320@gmail.com>
2022-12-09 20:30:43 -0300

    arrumei o alinhamento do menu e coloquei a cor no botao

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import React from "react";
 export default function Header(){
    return (
    <div>
-      <header style={{display:"flex" ,flexDirection:"column" ,padding:"10px 30px"}}>
-         <div style={{padding:"34px 100px",flexDirection:"row" ,display:"flex" ,alignItems:"center" ,justifyContent:"space-around" ,border:"2px solid black"}}>
-            <img style={{width:"33px" ,height:"33px" ,top:"38px" ,margin:"8px ",}} src="/img/logodigital.png" />
-            <div style={{width:"212px" ,height:"44px" ,left:"41px" ,fontSize:"36.4px" ,letterSpacing:"-0.01em" ,color:"#C92071" }}>Digital Store</div>
-            <input placeholder="Buscar" style={{width:"40%" ,heigth:"20px" ,padding:"20px" ,marginLeft:"27px" ,marginRight:"48px"}}></input>
+      <header style={{display:"flex" ,flexDirection:"column"  }}>
+         <div style={{padding:"34px 102px",flexDirection:"row" ,display:"flex" ,alignItems:"center" ,justifyContent:"space-between" }}>
+            <img style={{width:"33px" ,height:"33px" ,top:"38px" ,marginRight:"8px "}} src="/img/logodigital.png" />
+            <div style={{width:"212px" ,height:"44px" ,left:"41px" ,fontSize:"36.4px" ,letterSpacing:"-0.01em" ,color:"#C92071" ,fontWeight:"600"}}>Digital Store</div>
+            <input placeholder="Pesquisar produto..." style={{width:"40%" ,heigth:"20px" ,padding:"20px" ,marginLeft:"27px" ,marginRight:"48px" ,fontWeight:"400" ,}}></input>
             <button style={{width:"102px" ,lineHeight:"28px" ,fontSize:"16px" ,textDecorationLine:"underline" ,border:"none"}}>Cadastre-se</button>
-            <button style={{margin:"0px 10px" ,padding:"0px" }}>Entrar</button>
+            <button style={{width:"114px" ,height:"40px" ,borderRadius:"8px" ,background:"#C92071" ,color:"white"}}>Entrar</button>
             <img style={{width:"33px" ,height:"33px" ,top:"38px" ,margin:"8px " ,}} src="/img/carrinho.png" />
          </div>
          <div style={{display:"flex" ,alignItems:"center" ,marginRight:"30px"}}>
-            <span style={{marginRight:"30px"}}>Home</span>
+            <span style={{marginLeft:"102px" ,marginRight:"30px"}}>Home</span>
             <span style={{marginRight:"30px"}}>Produtos</span>
             <span style={{marginRight:"30px"}}>Categorias</span>
             <span style={{marginRight:"30px"}}>Meus Pedidos</span>
@@ -21,17 +21,6 @@ export default function Header(){
       </header>
    </div>
 
-// Width
-// 559px
-// Height
-// 60px
-// Top
-// 34px
-// Left
-// 384px
-// Radius
-// 8px
-// Opacity
-// 4%n
+
    );
    }
