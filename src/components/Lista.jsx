33a6923cd66876ab239116dbdc0c2886122b51d7
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2022-12-15 14:23:51 -0300

    Add files via upload

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,3 +1,28 @@
-export default function Lista(){
-    return <span>Lista Andre<p></p></span> 
- }
+import React from "react"
+import ListaCards from "./ListaCards"
+
+export const Lista = () => {
+    return (
+        <div>
+            <h3>Produtos em alta</h3>
+            <section>
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+                <ListaCards />
+            </section>
+        </div>
+    //  style={{ display: 'inline-block' }}
+    )
+}
