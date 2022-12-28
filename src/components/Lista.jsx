947e0e73c2943195c690f2b1b1686ee6daa2ff09
andresoaresmota <andresoaresmota@gmail.com>
2022-12-28 01:30:54 -0300

    recuperando?

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,28 +1,3 @@
-import React from "react"
-import ListaCards from "./ListaCards"
-
-export const Lista = () => {
-    return (
-        <div>
-            <h3>Produtos em alta</h3>
-            <section>
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-                <ListaCards />
-            </section>
-        </div>
-    //  style={{ display: 'inline-block' }}
-    )
-}
+export default function Lista(){
+    return <span>Lista<p></p></span> 
+ }
