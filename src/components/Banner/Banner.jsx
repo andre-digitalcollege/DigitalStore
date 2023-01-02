57067111495b896cba5034ba4316a6789e891702
Helio <jhelioneto@hotmail.com>
2023-01-02 11:25:34 -0300

    Adicionado carrossel a estrutura do Banner, e resolvido algumas incompatibilidades

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,22 +2,42 @@ import React from 'react'
 import './banner.css'
 import WhiteSneakers from './img/White-Sneakers-PNG-Clipart 1.png'
 import Ornament11 from './img/Ornament 11.png'
-import Carousel from './carousel'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function Banner() {
     return (
-        <div className="container">
-            <img src={Ornament11} alt="Ornament" className='ornamento-11' />
-            <img src={WhiteSneakers} alt="Shoe" className='shoe' />
-            <p className='mop' >Melhores ofertas personalizadas</p>
-            <p className='qsn'>Queima de stoque Nike 🔥</p>
-            <p className='texto'>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur</p>
-            <button className='bnt-verOferta'>Ver Oferta</button>
-            <div className="carousel">
-                <div className="">
+        <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+        >
+            <SwiperSlide>
+                <div className="container">
+                    <img src={Ornament11} alt="Ornament" className='ornamento-11' />
+                    <img src={WhiteSneakers} alt="Shoe" className='shoe' />
+                    <p className='mop' >Melhores ofertas personalizadas</p>
+                    <p className='qsn'>Queima de stoque Nike 🔥</p>
+                    <p className='texto'>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur</p>
+                    <button className='bnt-verOferta'>Ver Oferta</button>
+                    <div className="carousel">
+                        <div className="">
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <Carousel />
-        </div>
+            </SwiperSlide>
+            <SwiperSlide>BANNER 2</SwiperSlide>
+            <SwiperSlide>BANNER 3</SwiperSlide>
+            <SwiperSlide>BANNER 4</SwiperSlide>
+        </Swiper>
     );
 }
