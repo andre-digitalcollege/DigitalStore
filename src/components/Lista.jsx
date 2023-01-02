c674475ae368acc4d4166988c6e2846dfc6a109e
nivea-natalia <ninarodriguesadm+digitalcollege@gmail.com>
2023-01-02 20:16:54 -0300

    lista de produtos Nívea parcial

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,5 +1,172 @@
 export default function Lista(){
-    return  
+    return  (
+        <section>
+        <div >
+            <p style= {{/* Produtos em alta */
+
+position: 'absolute',
+width: '210px',
+height: '338px',
+left: '100px',
+
+
+/* Text Large Bold */
+fontFamily: 'Inter',
+fontStyle: 'normal',
+fontWeight: '700',
+fontSize: '24px',
+lineHeight: '38px',
+/* identical to box height, or 158% */
+letterSpacing: '0.75px',
+
+/* dark-gray-2 */
+color: '#474747',
+
+}}>Produtos em alta </p> 
+            <div /*style= {{fundo
+boxSizing: 'border-box',
+position: 'absolute',
+width: '292px',
+height: '321px',
+left: '100px',
+top: '1699px',
+
+background: '#FFFFFF',
+boxShadow: '6px 16px 30px rgba(105, 98, 98, 0.05)',
+borderRadius: '4px',
+}}*/ >
+                <span style= {{
+/*30% off fundo*/
+display: 'flex',
+flexDirection: 'row',
+justifyContent: 'center',
+alignItems: 'center',
+padding: '5px 15px',
+gap: '10px',
+
+position: 'absolute',
+
+left: '120px',
+top: '1719px',
+
+background: '#E7FF86',
+borderRadius: '29px',
+/* 30% OFF */
+width: '66px',
+height: '22px',
+
+fontFamily: 'Inter',
+fontStyle: 'normal',
+fontWeight: '700',
+fontSize: '14px',
+lineHeight: '22px',
+
+letterSpacing: '0.75px',
+
+color: '#474747',
+
+flex: 'none',
+order: '0',
+flexGrow: '0',
+
+                }}>30% OFF</span>
+                <div>figura</div>
+                <p style={{
+                    /* Tênis */
+position: 'absolute',
+width: '35px',
+height: '24px',
+left: '100px',
+top: '2038px',
+
+/* Text Tinny Bold */
+fontFamily: 'Inter',
+fontStyle: 'normal',
+fontWeight: '700',
+fontSize: '12px',
+lineHeight: '24px',
+/* identical to box height, or 200% */
+letterSpacing: '0.75px',
+
+/* light-gray */
+color: '#8F8F8F',
+                }}>Tênis</p>
+                <div style={{
+                     /* K-Swiss V8 - Masculino */
+position: 'absolute',
+width: '283px',
+height: '38px',
+left: '100px',
+top: '2062px',
+
+/* Text Large - Desktop */
+fontFamily: 'Inter',
+fontStyle: 'normal',
+fontWeight: '400',
+fontSize: '24px',
+lineHeight: '38px',
+/* identical to box height, or 158% */
+letterSpacing: '0.75px',
+
+/* dark-gray-2 */
+color: '#474747',
+
+
+                }}>K-Swiss V8 - Masculino</div>
+                <div style={{
+/* $200 */
+
+/* Text Large - Desktop */
+fontFamily: 'Inter',
+fontStyle: 'normal',
+fontWeight: '400',
+fontSize: '24px',
+lineHeight: '38px',
+/* identical to box height, or 158% */
+letterSpacing: '0.75px',
+
+/* light-gray */
+color: '#8F8F8F',
+
+/* Line 1 */
+position: 'absolute',
+width: '69px',
+height: '0px',
+left: '100px',
+top: '2121px',
+
+/* light-gray */
+background: '#8F8F8F',
+/* light-gray */
+border: '1px solid #8F8F8F',
+                }}>$200</div>
+                <div style={{
+                    /* $100 */
+position: 'absolute',
+width: '63px',
+height: '38px',
+left: '177px',
+top: '2100px',
+
+/* Text Large Bold */
+fontFamily: 'Inter',
+fontStyle: 'normal',
+fontWeight: '700',
+fontSize: '24px',
+lineHeight: '38px',
+/* identical to box height, or 158% */
+letterSpacing: '0.75px',
+
+/* dark-gray */
+color: '#1F1F1F',
+
+
+                }}>$100</div>
+            </div>
+        </div>
+    </section>
+    )
  }
+ 
 
  
