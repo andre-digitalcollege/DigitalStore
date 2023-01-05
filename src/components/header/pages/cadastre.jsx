6a62ffe3aa93a7eb04fbe6818172ecc0080ff4b8
blindao <rianrafaellimapinheiror@gmail.com>
2023-01-05 17:16:00 -0300

    tela de cadastro atualização

--- src/components/header/pages/cadastre.jsx
+++ src/components/header/pages/cadastre.jsx
@@ -2,21 +2,23 @@ import Footer from "../../Footer.jsx";
 import Logo from "../img/logo.png"
 
 
-function Cadastre (){
-    return(
-        <div className="lineargradient">
+function Cadastre() {
+    return (
+        <div>
             <header className="headercadastre">
-            <div className='Digitalcadastre' >
-            <img src={Logo} alt='shoe' style={{ width: '22.5px',height:'22.5px',top:'25.91px' }}></img>
-            <span>Digital Store</span>
-            </div>
+                <div className='Digitalcadastre' >
+                    <img src={Logo} alt='shoe' className="imgcadastre" style={{ width: '22.5px', height: '22.5px', top: '25.91px' }}></img>
+                    <span className="digitalcadastrenome">Digital Store</span>
+                </div>
             </header>
-            
-            <div style={{width: '583px', height:'560px', paddingTop:'200px', paddingLeft:'100px',borderRadius:'4px',padding:'30px',gap:'30px',} }>
+            <div className="lineargradient">
+                <div className="telinhaa">
+                </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
-        
+
+
     )
 }
 export default Cadastre
