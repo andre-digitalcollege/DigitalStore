4069f223c76e0024fe841d4b37ddb9c7163f42e3
Wesley Azevedo <azevedosou@gmail.com>
2023-01-07 00:46:18 -0300

    conclusao banner destaque

--- src/components/BannerDestaque/Destaques.jsx
+++ src/components/BannerDestaque/Destaques.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import imgBanner from "../../img/bannerDestaque/imgBannerDestaque.svg";
+import imgBanner from "../../img/bannerDestaque/imagemBanner.png";
 import './destaque.css'
 
 function Destaques() {
   return (
-    <div>
-        <div >
-            <img className="imagem" src={imgBanner} alt="Imagem banner destaque"/>
+    <div className='predefinicoesDestaque'>
+        <div className='containerImagem'>
+            <img className="img" src={imgBanner} alt="Imagem banner destaque"/>
         </div>
         <div className="informacoes">
-            <p>Oferta especial</p>
-            <p>Air Jordan edição de colecionador</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+            <p className='textoDestaque'>Oferta especial</p>
+            <p className='tituloProduto'>Air Jordan edição de colecionador</p>
+            <p className='descricaoProduto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
             <button>Ver Oferta</button>
         </div>
     </div>
